1ea3518aad0fa30018e519314e53e54d893bb698
Jay <jay_on_business@outlook.com>
2023-05-14 12:37:18 +0800

    20230514

---
+++ cloud-demo/gateway/src/main/java/cn/mycloudway/gateway/AuthorizeFilter.java
@@ -0,0 +1,34 @@
+package cn.mycloudway.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+//@Order(-1)
+@Component
+public class AuthorizeFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        HttpHeaders headers = request.getHeaders();
+        String auth = headers.getFirst("authorization");
+
+        if ("admin".equals(auth)) {
+            return chain.filter(exchange);
+        }
+
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}

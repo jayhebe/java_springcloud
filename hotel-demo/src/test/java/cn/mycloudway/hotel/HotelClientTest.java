e56ac17341a2dfa71f6208ff5ebc4336141dd379
Jay <jay_on_business@outlook.com>
2023-05-21 00:28:40 +0800

    20230521

--- hotel-demo/src/test/java/cn/mycloudway/hotel/HotelClientTest.java
+++ hotel-demo/src/test/java/cn/mycloudway/hotel/HotelClientTest.java
@@ -1,13 +1,18 @@
 package cn.mycloudway.hotel;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+
+import static cn.mycloudway.hotel.constant.HotelConstants.MAPPING_TEMPLATE;
 
 public class HotelClientTest {
     private RestHighLevelClient client;
@@ -15,6 +20,13 @@ public class HotelClientTest {
     @Test
     void testInit() {
         System.out.println(client);
+    }
+
+    @Test
+    void createHotelIndex() throws IOException {
+        CreateIndexRequest request = new CreateIndexRequest("hotel");
+        request.source(MAPPING_TEMPLATE, XContentType.JSON);
+        client.indices().create(request, RequestOptions.DEFAULT);
     }
 
     @BeforeEach

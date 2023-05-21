bb5a2965ceff948e9abed9984b21f4ac3d4ef741
Jay <jay_on_business@outlook.com>
2023-05-21 23:11:29 +0800

    20230521

--- hotel-demo/src/test/java/cn/mycloudway/hotel/HotelClientTest.java
+++ hotel-demo/src/test/java/cn/mycloudway/hotel/HotelClientTest.java
@@ -1,10 +1,12 @@
 package cn.mycloudway.hotel;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,10 +25,24 @@ public class HotelClientTest {
     }
 
     @Test
-    void createHotelIndex() throws IOException {
+    void testCreateHotelIndex() throws IOException {
         CreateIndexRequest request = new CreateIndexRequest("hotel");
         request.source(MAPPING_TEMPLATE, XContentType.JSON);
         client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testDeleteHotelIndex() throws IOException {
+        DeleteIndexRequest request = new DeleteIndexRequest("hotel");
+        client.indices().delete(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testExistsHotelIndex() throws IOException {
+        GetIndexRequest request = new GetIndexRequest("hotel");
+        boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
+
+        System.out.println(exists ? "索引库存在" : "索引库不存在");
     }
 
     @BeforeEach
---
+++ hotel-demo/src/test/java/cn/mycloudway/hotel/HotelDocumentTest.java
@@ -0,0 +1,27 @@
+package cn.mycloudway.hotel;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
+
+public class HotelDocumentTest {
+    private RestHighLevelClient client;
+
+
+
+    @BeforeEach
+    void setUp() {
+        this.client = new RestHighLevelClient(RestClient.builder(
+                HttpHost.create("http://localhost:9200")
+        ));
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        this.client.close();
+    }
+}

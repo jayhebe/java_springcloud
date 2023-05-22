962865335331589928478a2fead01417601cc8f1
Jay <jay_on_business@outlook.com>
2023-05-22 23:56:28 +0800

    20230522

--- hotel-demo/src/test/java/cn/mycloudway/hotel/HotelDocumentTest.java
+++ hotel-demo/src/test/java/cn/mycloudway/hotel/HotelDocumentTest.java
@@ -1,17 +1,84 @@
 package cn.mycloudway.hotel;
 
+import cn.mycloudway.hotel.pojo.Hotel;
+import cn.mycloudway.hotel.pojo.HotelDoc;
+import cn.mycloudway.hotel.service.IHotelService;
+import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.List;
 
+
+@SpringBootTest
 public class HotelDocumentTest {
+    @Autowired
+    private IHotelService hotelService;
     private RestHighLevelClient client;
 
+    @Test
+    void testAddDocument() throws IOException {
+        Hotel hotel = hotelService.getById(36934L);
+        HotelDoc hotelDoc = new HotelDoc(hotel);
 
+        IndexRequest request = new IndexRequest("hotel").id(hotel.getId().toString());
+        request.source(JSON.toJSONString(hotelDoc), XContentType.JSON);
+        client.index(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testBulkAddDocument() throws IOException {
+        BulkRequest request = new BulkRequest();
+
+        List<Hotel> hotelList = hotelService.list();
+
+        for (Hotel hotel : hotelList) {
+            HotelDoc hotelDoc = new HotelDoc(hotel);
+            request.add(new IndexRequest("hotel")
+                    .id(hotel.getId().toString())
+                    .source(JSON.toJSONString(hotelDoc), XContentType.JSON));
+        }
+
+        client.bulk(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testGetDocumentById() throws IOException {
+        GetRequest request = new GetRequest("hotel", "36934");
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        System.out.println(response.getSourceAsString());
+    }
+
+    @Test
+    void testUpdateDocumentById() throws IOException {
+        UpdateRequest request = new UpdateRequest("hotel", "36934");
+        request.doc(
+                "price", "852",
+                "starName", "四钻"
+        );
+        client.update(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testDeleteDocumentById() throws IOException {
+        DeleteRequest request = new DeleteRequest("hotel", "36934");
+        client.delete(request, RequestOptions.DEFAULT);
+    }
 
     @BeforeEach
     void setUp() {
